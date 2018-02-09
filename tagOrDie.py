f1b75d5235af4d644efc7cf6a2996e96d2a22d48
Wes <wes.coffay@gmail.com>
2018-02-08 20:02:13 -0500

    added for all variables

--- tagOrDie.py
+++ tagOrDie.py
@@ -4,24 +4,26 @@ import boto3
 
 # i-0a0f29c2e338fc66f is my sample
 
-#identify instance
+# identify instance
 newInstance = client.describe_instances(InstanceIds = ["i-0a0f29c2e338fc66f"])
 print newInstance
 
-#get tags attached to instance
+# get tags attached to instance
 print newInstance["Reservations"][0]["Instances"][0]["Tags"]
-#turn those into a dict
+# turn those into a dict
 myTagDict = {}
 for pair in newInstance["Reservations"][0]["Instances"][0]["Tags"]:
     myTagDict[pair["Key"]] = pair["Value"]
 print myTagDict
 
-#instantiate approved values:
+# instantiate approved values:
 approvedUsers = ("Wes", "Kate", "Bill")
 approvedNames = ("Scarif", "Sandbox", "Jakku", "Stardust")
 approvedDepts = ("Monk", "Cosmotology", "Dwellers", "Catnip", "R&D")
 
-#check if required values are present in the dict
+# check if required values are present in the dict
+# REFACTOR INTO LIST AND LOOP
+# User
 if "User" in myTagDict:
     # check if required values are valid
     if myTagDict["User"] in approvedUsers:
@@ -30,3 +32,27 @@ if "User" in myTagDict:
         print "User not valid"
 else:
     print "No User Specified"
+
+# Name
+if "Name" in myTagDict:
+    # check if required values are valid
+    if myTagDict["Name"] in approvedNames:
+        print myTagDict["Name"]
+    else:
+        print "Name not valid"
+else:
+    print "No Name Specified"
+
+# Dept
+if "Dept" in myTagDict:
+    # check if required values are valid
+    if myTagDict["Dept"] in approvedDepts:
+        print myTagDict["Dept"]
+    else:
+        print "Dept not valid"
+else:
+    print "No Dept Specified"
+
+# if any error returns, sent command to stop instance
+
+# should we create tags with null values?
